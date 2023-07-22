9ff52183b7af75011ba00a74204cc6bd2bbd12e1
Megas-MDN <mdn.melqui@gmail.com>
2023-07-22 14:37:51 -0300

    set env file

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const io = require('socket.io')(server, {
   },
 });
 
-const CHAT = 'newChatMessage';
+const CHAT = process.env.CHAT_NAME;
+const CHAT_GLOBAL = process.env.GLOBAL_CHAT;
 
 io.on('connection', (socket) => {
   const { roomId } = socket.handshake.query;
   console.log('Socket id :: %s :: room :: %s ::', socket.id, roomId);
 
-  socket.join(roomId);
+  if (roomId) {
+    socket.join(roomId);
+  }
 
   socket.on(CHAT, (data) => {
     io.in(roomId).emit(CHAT, data);
+  });
+
+  socket.on(CHAT_GLOBAL, (data) => {
+    socket.emit(CHAT_GLOBAL, data);
+    socket.broadcast.emit(CHAT_GLOBAL, data);
   });
 
   socket.on('disconnect', (reason) => {
